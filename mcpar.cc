5a489e0bdb6fc5d65af3a4c07c11696a9f96586b
Robert Link <robert.link@pnl.gov>
2011-03-08 14:53:30 -0500

    Further development on parallel MCMC.

--- mcpar.cc
+++ mcpar.cc
@@ -7,12 +7,16 @@ namespace mcp {
 
   enum {OK, INVALID, ERROR};
   
-  int mcpar(MCparm &parms, float *y, VLFunc &L, MCout &outsamples,
+  int mcpar(int nsamp, int nburn, MCparm &parms, float *y, VLFunc &L, MCout &outsamples,
             MPIWrapper *mpi, float *incov)
   {
     int nparm  = parms.nparm();
     int nchain = parms.nchain();
+    int ntot   = nparm*nchain;
     int rank   = mpi ? mpi.rank() : 0;
+    int size   = mpi ? mpi.size() : 1; // number of MPI processes
+    int tsize  = size*nchain;          // total number of chains across all processes
+    
 
     // set up the covariance matrix.  Copy incov if it was provided; otherwise use identity matrix
     int ncov   = nparam*nparam;
@@ -43,17 +47,76 @@ namespace mcp {
         
 
     // here are the things we will need to do the monte carlo
-    float *trial = new float[nparm]; // trial parameters
-    float *mu    = new float[nparm]; // parameter means - incremental update
-    float *sig   = new float[nparm]; // parameter stdevs - incremental update
-    float scale  = 1.0f;
-    int   ntrial = 0;
-    int  naccept = 0;
+    float *pvals  = new float[ntot]; // parameter values
+    float *ptrial = new float[ntot]; // trial parameters
+    float *mu     = new float[ntot]; // parameter means - incremental update
+    float *sig    = new float[ntot]; // parameter stdevs - incremental update
+    floag *lylast = new float[nchain]; // previous log-likelihood values
+    float *lytrial = new float[nchain]; // trial log-likelihood values
+    float *pacpt  = new float[nchain]; // acceptance probability
+    float *acpt   = new float[nchain]; // test for acceptance
+    float scale   = 1.0f;
+    float ntrial  = 0.0f;
+    float naccept = 0.0f;
     
+    // copy input guesses to our working array
+    for(int j=0;j<ntot;++j)
+      pvals[j] = parms.pvals[j];
+    
+    // first step - burn in.  Run the MC, but don't perform any remote updates
+    int irate = nburn/10;
+    for(int i = 0; i<nburn; ++i) {
+      genLocal(nparm, nchain, parms, rng, ltrial);
+      L(ltrial, ytrial);
 
-    
+      // XXX generate acpt from rng
+      
+      for(int j=0; j<nchain; ++j) {
+        pacpt[j] = exp(ytrial[j]-ylast[j]);
+        ylast[j] = acpt[j]<pacpt[j] ? ytrial[j] : ylast[j];
+      }
+
+      ntrial += nchain;
+      for(int j=0; j<nchain; ++j) {
+        int idx = j*nparm;
+        if(acpt[j] < pacpt[j]) {
+          naccept += 1.0f;
+          for(int k=0; k<nparm; ++k,++idx)
+            pvals[idx] = ptrial[idx];
+        }
+      }
+
+      // tune acceptance rate
+      if(i>irate) {
+        float arate = naccept/ntrial;
+        if(arate < TGT_ARATE_MIN) {
+          naccept = ntrial = 0.0f; // reset the counters if we change size
+          for(j=0;j<ncov;++j)
+            cov[j] *= STEP_DEC;
+        }
+        else if(arate > TGT_ARATE_MAX) {
+          naccept = ntrial = 0.0f; // reset counters
+          for(j=0;j<ncov;++j)
+            cov[j] *= STEP_INC;
+        } 
+        // evaluate again in 50 steps
+        irate += 50;
+      } 
+    }
+
+    // run the "keeper" samples, including remote proposals.  To keep
+    // the calc well-vectorized, we do one check for whether to take a
+    // local proposal or a remote one and apply it to all of the
+    // chains in this process.
+    for(int i=0; i<nsamp; ++i)
     
     // cleanup
+    delete [] acpt;
+    delete [] pacpt;
+    delete [] sig;
+    delete [] mu;
+    delete [] ptrial;
+    delete [] pvals;
     vsldeletestream(rng);
     delete [] cov;
   }
